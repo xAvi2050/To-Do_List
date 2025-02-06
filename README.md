15ac7af441b55390ab7783105c394ad26fcc8570
xAvi2050 <cheetahmindset7@gmail.com>
2025-02-06 23:17:42 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,16 @@
+
+This is a simple to-do list application that allows users to add, check, and delete tasks. The tasks persist using localStorage, so they remain saved even after the page is reloaded.
+
+Features:-
+<br>
+→ Add tasks to the list. <br>
+→ Mark tasks as completed. <br>
+→ Remove tasks when no longer needed. <br>
+→ Saves tasks in localStorage for persistence. <br>
+
+How to Use:-
+<br>
+→ Enter a task in the input box and press the Add button. <br>
+→ Click on a task to mark it as completed. <br>
+→ Click on the "x" icon to remove a task. <br>
+→ The list is saved in localStorage, so it remains even after refreshing the page <br>
---
+++ app.js
@@ -0,0 +1,40 @@
+
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("Please add your task");
+    }
+    else{
+        let newList = document.createElement("li");
+        newList.innerHTML = inputBox.value;
+        listContainer.appendChild(newList);
+
+        let cross = document.createElement("span");
+        cross.innerHTML = `x`;
+        newList.appendChild(cross);
+    }
+    inputBox.value = "";
+    savaData();
+}
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked"); // create a class (named checked; then automatically, the CSS will apply on that class)
+        savaData();
+    }else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        savaData();
+    }
+});
+
+function savaData(){
+    localStorage.setItem("Data", listContainer.innerHTML); // key, value
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("Data");
+}
+
+showTask();
